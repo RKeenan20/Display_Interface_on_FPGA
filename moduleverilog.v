003c1ab254cbe1180177fe9db16a1a7626af1ab8
Robert Keenan <>
2017-10-19 12:48:34 +0100

    Final Verilog Module

--- moduleverilog.v
+++ moduleverilog.v
@@ -35,11 +35,11 @@ module DisplayInterface(input clk5,
 												input reset,
 												input [15:0] dispVal,
 												output reg [7:0] digit,
-												output reg [7:0] segment
+												output [7:0] segment
 												);
 
-					localparam  compare = 624; //Want to output on 624th clock cycle = 4kHz
-          reg [9:0] countCD;
+					localparam  compare = 5; //Want to output on 624th clock cycle = 4kHz
+                    reg [10:0] countCD;
 					reg [1:0] counterDisplay;
 					reg Enable;
 					reg [3:0] hexOutput;
@@ -49,11 +49,13 @@ module DisplayInterface(input clk5,
           always @(posedge clk5)
           	begin
           			if(reset)
-              		countCD <= 10'b0;
+              		countCD <= 11'b0;
                 else if (countCD == compare) //Once it hits 624, set back to zero to count again
-                  countCD <= 10'b0;
+                  countCD <= 11'b0;
+
                 else
                   countCD <= countCD + 1'b1;
+
           	end
 
           always @(posedge clk5)
@@ -61,13 +63,13 @@ module DisplayInterface(input clk5,
                 if(reset)
                 	Enable <= 1'b0;
                 else if(countCD == compare) //Inverts the output from the last 624th clock cycle
-                	Enable <= ~Enable;
+                	Enable <= 1'b1;
                 else //Else havent hit the 624th edge yet, so hold value
-                  Enable <= Enable;
+                    Enable <= 1'b0;
           	end
 
-					//Now to the actually displaying the values to the screen
-					//2 bit counter for controlling input to MUX
+					//Now to actually displaying the values to the screen
+					//2 bit counter for controlling input to MUXs
 					always @(posedge clk5)
 						begin
 							if(reset)
@@ -79,13 +81,19 @@ module DisplayInterface(input clk5,
 						end
 
 					//Segment MUX
-					always @(counterDisplay)
+					always @(counterDisplay, dispVal)
 						case(counterDisplay)
-							2'b00: hexOutput = dispVal[3:0];
+							2'b00: hexOutput [3:0] = dispVal[3:0];
 							2'b01: hexOutput = dispVal[7:4];
 							2'b10: hexOutput = dispVal[11:8];
 							2'b11: hexOutput = dispVal[15:12];
 						endcase
+
+
+						hex2seg seg1 (.number (hexOutput[3:0]),           // 4-bit number
+                          .pattern (segment[7:1])
+                          );
+
 					//Digit Display MUX
 					always @(counterDisplay)
 						case(counterDisplay)
@@ -95,9 +103,7 @@ module DisplayInterface(input clk5,
 							2'b11: digit = 8'b11110111;
 						endcase
 
-					hex2seg seg1 (.number (hexOutput),           // 4-bit number
-												.pattern (segment[7:1])
-												);
+
 
 
 endmodule
